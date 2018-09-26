e13bf86cce9d94bb9b453287aa74357bef9e3ec0
Marek Magdziak <marek.magdziak@hotmail.cz>
2018-09-26 21:26:29 +0200

    refactor

---
+++ Mkm.GraphQL.Client/Attributes/GraphQLFieldAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace Mkm.GraphQL.Client.Attributes
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Method)]
+    public class GraphQLFieldAttribute : Attribute
+    {
+        public GraphQLFieldAttribute(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; }
+    }
+}
---
+++ Mkm.GraphQL.Tooling/ClassGenerator/DefinitionHandlers/EnumTypeDefinitionHandler.cs
@@ -0,0 +1,24 @@
+﻿using GraphQLParser.AST;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Mkm.GraphQL.Tooling.CodeGenerator.DefinitionHandlers
+{
+    public class EnumTypeDefinitionHandler : IDefinitionHandler
+    {
+        public NamespaceDeclarationSyntax Handle(ASTNode definition, NamespaceDeclarationSyntax @namespace)
+        {
+            var enumTypeDefinition = definition as GraphQLEnumTypeDefinition;
+
+            var enumDeclaration = SyntaxFactory.EnumDeclaration(enumTypeDefinition.Name.Value)
+                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword));
+
+            foreach (var value in enumTypeDefinition.Values)
+            {
+                enumDeclaration = enumDeclaration.AddMembers(SyntaxFactory.EnumMemberDeclaration(value.Name.Value));
+            }
+
+            return @namespace.AddMembers(enumDeclaration);
+        }
+    }
+}
